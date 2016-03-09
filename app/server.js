c4b68fb425ebe9c0b1ee7fef4239c1feec461ae2
Hervé Tutuaku <htutuaku@WKS-DEBIAN-01>
2016-03-10 00:18:54 +0100

    [feature] switch to exprss server

--- app/server.js
+++ app/server.js
@@ -1,10 +1,17 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+
+app.use("/styles", express.static(__dirname + '/styles'));
+app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use("/images", express.static(__dirname + '/images'));
+app.use("/data", express.static(__dirname + '/data'));
+app.use("/vendor", express.static(__dirname + '/vendor'));
 
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.sendfile('socket.html');
+  res.sendFile(__dirname + '/index.html');
 
   var dataBag = {};
 
@@ -26,6 +33,6 @@ app.get('/', function(req, res) {
 	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8000, function(){
+  console.log('listening on *:8000');
 });
